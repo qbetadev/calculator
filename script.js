86eb2517126b316345e9c9b8ebc34ea9a28be59b
qbetadev <157713329+qbetadev@users.noreply.github.com>
2024-08-02 22:26:12 +0300

    Improve UI, add all calculator functions

--- script.js
+++ script.js
@@ -1,3 +1,81 @@
+const numberButtons = document.querySelectorAll('.button_number');
+const operatorButtons = document.querySelectorAll('.button_operate');
+const clearButton = document.querySelector('.button_clear');
+const deleteButton = document.querySelector('.button_delete');
+const pointButton = document.querySelector('.button_point');
+const equalButton = document.querySelector('.button_equal');
+const currentOperationDisplay = document.querySelector('.display_current-operation');
+const currentNumberDisplay = document.querySelector('.display_current-number');
+
+let operandFirst = '';
+let operandSecond = '';
+let currentOperator = '';
+
+let shouldResetCurrentNumber = false;
+
+numberButtons.forEach((button) => {
+    button.addEventListener('click', () => appendNumber(button.textContent));
+});
+
+operatorButtons.forEach((button) => {
+    button.addEventListener('click', () => setCurrentOperation(button.textContent));
+});
+
+clearButton.addEventListener('click', () => clearDisplay());
+
+deleteButton.addEventListener('click', () => deleteNumber());
+
+pointButton.addEventListener('click', () => appendNumber(pointButton.textContent));
+
+equalButton.addEventListener('click', () => evaluate());
+
+function appendNumber(string) {
+    if (currentNumberDisplay.textContent == 0 || shouldResetCurrentNumber) {
+        resetDisplay();
+        shouldResetCurrentNumber = false;
+        currentNumberDisplay.textContent += string;
+    } else {
+        currentNumberDisplay.textContent += string;
+    }
+}
+
+function clearDisplay() {
+    currentNumberDisplay.textContent = 0;
+    currentOperationDisplay.textContent = '';
+}
+
+function resetDisplay() {
+    currentNumberDisplay.textContent = '';
+}
+
+function deleteNumber() {
+    if (!shouldResetCurrentNumber) {
+        currentNumberDisplay.textContent = currentNumberDisplay.textContent
+        .toString()
+        .slice(0, -1);
+    }
+    if (currentNumberDisplay.textContent == '') {
+        currentNumberDisplay.textContent = '0';
+    }
+}
+
+function setCurrentOperation(operator) {
+    if (currentOperator !== '') evaluate()
+    operandFirst = currentNumberDisplay.textContent;
+    currentOperator = operator;
+    currentOperationDisplay.textContent = `${operandFirst} ${currentOperator}`;
+    shouldResetCurrentNumber = true;
+}
+
+function evaluate() {
+    if (currentOperator == '' || shouldResetCurrentNumber) return;
+    operandSecond = currentNumberDisplay.textContent;
+    currentOperationDisplay.textContent = `${operandFirst} ${currentOperator} ${operandSecond} =`;
+    currentNumberDisplay.textContent = operate(operandFirst, operandSecond, currentOperator);
+    currentOperator = '';
+    shouldResetCurrentNumber = true;
+}
+
 function add(number1, number2) {
     return number1 + number2;
 }
@@ -22,27 +100,17 @@ function convertToFloat(string) {
     return parseFloat(string);
 }
 
-function operate() {
-    const operators = '+-*/';
-    let expression = prompt('Enter the expression');
-    let operator = findOperator(expression.split(''), operators);
-    
-    let result = expression
-    .split(operator)
-    .map(convertToFloat)
-    .reduce((result, currentItem) => {
-        switch (operator) {
-            case '+':
-                return add(result, currentItem);
-            case '-':
-                return subtract(result, currentItem);
-            case '*':
-                return multiply(result, currentItem);
-            case '/':
-                return divide(result, currentItem);
-        }
-    })
-    console.log(result);
+function operate(operandFirst, operandSecond, currentOperator) {
+    operandFirst = parseFloat(operandFirst);
+    operandSecond = parseFloat(operandSecond);
+    switch(currentOperator) {
+        case '+':
+            return add (operandFirst, operandSecond);
+        case '-':
+            return subtract(operandFirst, operandSecond);
+        case '*':
+            return multiply(operandFirst, operandSecond);
+        case '/':
+            return divide(operandFirst, operandSecond);
+    }
 }
-
-operate();
